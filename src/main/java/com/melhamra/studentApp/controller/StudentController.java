875263e58fabfb857eaebbebf48e3c95f061ff90
melha <melhamra98@gmail.com>
2020-05-26 17:27:03 +0000

    commit message

--- src/main/java/com/melhamra/studentApp/controller/StudentController.java
+++ src/main/java/com/melhamra/studentApp/controller/StudentController.java
@@ -2,11 +2,14 @@ package com.melhamra.studentApp.controller;
 
 import com.melhamra.studentApp.model.Student;
 import com.melhamra.studentApp.repository.StudentRepository;
+import org.apache.commons.io.IOUtils;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -56,15 +61,33 @@ public class StudentController {
         if (bindingResult.hasErrors()){
             return "studentForm";
         }
-
         if(!multipartFile.isEmpty()){
             student.setPhoto(multipartFile.getOriginalFilename());
-            multipartFile.transferTo(new File(imageDir+multipartFile.getOriginalFilename()));
         }
-
         studentRepository.save(student);
+        if(!multipartFile.isEmpty()){
+            multipartFile.transferTo(new File(imageDir+student.getId()));
+        }
         return "redirect:/Student/Index";
     }
 
+    @GetMapping(value = "/getPhoto", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public byte[] getPhoto(@RequestParam(name = "id") Long id) throws IOException {
+        File file = new File(imageDir+id);
+        return IOUtils.toByteArray(new FileInputStream(file));
+    }
 
+    @RequestMapping("/delete")
+    public String deleteStudent(@RequestParam(name = "id") Long id){
+        studentRepository.deleteById(id);
+        return "redirect:/Student/Index";
+    }
+
+    @GetMapping("/edit")
+    public String editStudent(@RequestParam(name = "id") Long id,
+                                Model model){
+        model.addAttribute("student",studentRepository.getOne(id));
+        return "studentForm";
+    }
 }
