217054d201622b515777809c4bdb176ba0c37271
melha <melhamra98@gmail.com>
2020-05-26 13:59:43 +0000

    second commit

--- src/main/java/com/melhamra/studentApp/controller/StudentController.java
+++ src/main/java/com/melhamra/studentApp/controller/StudentController.java
@@ -3,15 +3,19 @@ package com.melhamra.studentApp.controller;
 import com.melhamra.studentApp.model.Student;
 import com.melhamra.studentApp.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -20,6 +24,8 @@ public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
+    @Value("${imagesDir}")
+    private String imageDir;
 
 
     @GetMapping("/Index")
@@ -36,5 +42,29 @@ public class StudentController {
         model.addAttribute("keyWord", keyWord);
         return "students";
     }
+
+    @GetMapping("/form")
+    public String studentForm(Model model){
+        model.addAttribute("student",new Student());
+        return "studentForm";
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@Valid Student student,
+                              BindingResult bindingResult,
+                              @RequestParam(name = "picture") MultipartFile multipartFile) throws IOException {
+        if (bindingResult.hasErrors()){
+            return "studentForm";
+        }
+
+        if(!multipartFile.isEmpty()){
+            student.setPhoto(multipartFile.getOriginalFilename());
+            multipartFile.transferTo(new File(imageDir+multipartFile.getOriginalFilename()));
+        }
+
+        studentRepository.save(student);
+        return "redirect:/Student/Index";
+    }
+
 
 }
--- src/main/java/com/melhamra/studentApp/model/Student.java
+++ src/main/java/com/melhamra/studentApp/model/Student.java
@@ -1,22 +1,35 @@
 package com.melhamra.studentApp.model;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.Date;
+
 
 @Data
 @Entity
-public class Student {
+@Validated
+public class Student implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty(message = "Should not be empty")
+    @Size(min = 5,max = 20, message = "The size must be between 5 and 20")
     private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateOfBirth;
+    @NotEmpty(message = "Should not be empty")
+    @Email(message = "Must be a syntactically correct email address")
     private String email;
     private String photo;
 
