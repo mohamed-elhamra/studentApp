b726dad3733e1b04849ccf478b8f390ca7abfd48
melha <melhamra98@gmail.com>
2020-05-27 13:26:22 +0000

    commit message

--- src/main/java/com/melhamra/studentApp/repository/StudentRepository.java
+++ src/main/java/com/melhamra/studentApp/repository/StudentRepository.java
@@ -3,14 +3,12 @@ package com.melhamra.studentApp.repository;
 
 import com.melhamra.studentApp.model.Student;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
